83166575d1c657ed7836790c78b9792cb1a27d01
Michael J Feher <michael.feher@algorand.foundation>
2026-02-04 08:51:59 -0500

    example: show working app

--- packages/@txnlab/use-wallet-react/src/hook.ts
+++ packages/@txnlab/use-wallet-react/src/hook.ts
@@ -4,17 +4,26 @@ import type {BaseProvider} from "@algorandfoundation/wallet-provider";
 import {Store} from "@tanstack/store";
 import {useStore} from "@tanstack/react-store";
 
+import {ReactNativeProvider} from "@perawallet/react-native-provider";
+
+// Get the context and api surface
 export function useWallet<P extends BaseProvider = BaseProvider>(){
     const provider = useContext(AlgorandContext) as P | null;
     if(provider === null) throw new Error('No Provider Found')
 
-    // Handle Store Hydration, this will be more robust in the future.
-    // ie. Object.entries to construct the result
+    // This could provide the default stores that evolve. (keystore, accounts, etc)
     let secrets = useStore((provider as any).$secrets as Store<any>, (s)=>s);
-
-
     return {
-        ...provider,
-        secrets
+        ...provider, secrets
     };
 }
+
+// Create implicit bindings to the state
+// export function useKeyStore(){
+//     const provider: ReactNativeProvider = useWallet()
+//     const secrets = useStore(provider.$secrets, (s)=>s);
+//     return {
+//         secrets,
+//         keystore: provider.keystore
+//     }
+// }
